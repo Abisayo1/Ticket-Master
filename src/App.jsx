52e133005154a5864ec3b4b2743f43f244b11efd
Abisayo Folu-Agoi <foluagoiabisayo@gmail.com>
2025-04-29 10:59:27 +0100

    Upload form Signed-off-by: Abisayo Folu-Agoi <foluagoiabisayo@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<UploadForm />} />
         <Route path='/uploadform' element={<UploadForm />} />
       </Routes>
     </BrowserRouter>
