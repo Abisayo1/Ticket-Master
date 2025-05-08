3045c0aa7b81e5967e03a23e6f3ef5cec1f3c0c5
Abisayo Folu-Agoi <foluagoiabisayo@gmail.com>
2025-05-08 08:44:53 +0100

    New update
    Signed-off-by: Abisayo Folu-Agoi <foluagoiabisayo@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { StaticRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <StaticRouter location={url}>
     <App />
-  </StrictMode>,
+  </StaticRouter>,
 )
