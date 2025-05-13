b284d3d3f4a8567227661c4d5d957bb99c08225c
Abisayo Folu-Agoi <foluagoiabisayo@gmail.com>
2025-05-13 07:48:36 +0100

    Pushed Secured by ticket master to the top a lil
    Signed-off-by: Abisayo Folu-Agoi <foluagoiabisayo@gmail.com>

--- src/pages/component/SecuredByTicketmaster.jsx
+++ src/pages/component/SecuredByTicketmaster.jsx
@@ -2,7 +2,7 @@ import { Lock } from 'lucide-react';
 
 export default function SecuredByTicketmaster() {
   return (
-    <div className="flex mt-7 mb-4 items-center justify-center">
+    <div className="flex mb-4 items-center justify-center">
       <div className="flex items-center space-x-2 text-sm">
         <Lock className="w-4 h-4 text-black" />
         <span className="text-gray-700">
